293bc6c2d8a60741f13821289f11fa9e508a87ee
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-03-09 16:50:06 -0700

    prompt is working and i'm able to manipulate the final answer object

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+inquirer
+  .prompt([
+    //title question
+    {
+      type: "input",
+      name: "title",
+      message: "How do you want to title this document?",
+      default: "Title Name",
+    },
+    //Licences list
+    {
+      type: "list",
+      name: "license",
+      message: "Please Select a License",
+      choices: ["License #1", "License #2", "License #3", "License #4"],
+      default: "License#1",
+    },
+  ])
+  .then((answers) => {
+    //function to generate README using MARKup and Dynamic variables
+    console.log(answers);
+  });
 // TODO: Create an array of questions for user input
 const questions = [];
 
